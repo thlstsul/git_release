1e6806adbf4e74f3f7ed48baf093c87173c633b6
THELOSTSOUL <1095533751@qq.com>
2025-10-05 23:47:07 +0800

    安装脚本

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name="git-release-tool",
+    version="0.1.0",
+    description="Git项目自动发布工具 - 自动更新版本号、提交、打标签、推送",
+    author="thlstsul",
+    author_email="zhouzm5@qq.com",
+    py_modules=["git_release"],
+    entry_points={
+        "console_scripts": [
+            "git-release=git_release:master",
+        ],
+    },
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
+)
